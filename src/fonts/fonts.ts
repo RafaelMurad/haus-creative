b5fa689c6a097ce3ef0d52ba6ee30fb23f320143
Rafael Murad <rflmurad@gmail.com>
2025-06-28 10:49:31 +0100

    Fix font loading by using web fonts

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,28 +1,8 @@
-import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
 
-export const inter = localFont({
-    src: [
-        {
-            path: '../app/fonts/Inter-Regular.woff2',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../app/fonts/Inter-Medium.woff2',
-            weight: '500',
-            style: 'normal',
-        },
-        {
-            path: '../app/fonts/Inter-SemiBold.woff2',
-            weight: '600',
-            style: 'normal',
-        },
-        {
-            path: '../app/fonts/Inter-Bold.woff2',
-            weight: '700',
-            style: 'normal',
-        },
-    ],
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
     variable: '--font-inter',
     display: 'swap',
 });
