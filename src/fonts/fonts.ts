b66ed52ae8c67d2dfb2f041e422ce135b81715a6
Rafael Murad <rflmurad@gmail.com>
2025-06-28 10:48:53 +0100

    Fix Google Fonts fetch error by using local fonts

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,7 +1,28 @@
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
-export const inter = Inter({
-    subsets: ['latin'],
-    display: 'swap',
+export const inter = localFont({
+    src: [
+        {
+            path: '../app/fonts/Inter-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../app/fonts/Inter-Medium.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../app/fonts/Inter-SemiBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../app/fonts/Inter-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
     variable: '--font-inter',
+    display: 'swap',
 });
